dde2b65a0af2348ba20e43b444ff17342d85dcf0
csrlop26 <csrlop26@gmail.com>
2026-05-25 00:02:58 +0200

    style: use user uploaded pizza images

--- src/data.ts
+++ src/data.ts
@@ -1,4 +1,8 @@
 import { PizzaItem, ReviewItem } from './types';
+import margheritaImg from './assets/pizzas/margherita.jpg';
+import pepperoniImg from './assets/pizzas/pepperoni.jpg';
+import carbonaraImg from './assets/pizzas/carbonara.jpg';
+import veganaImg from './assets/pizzas/vegana.png';
 
 export const PIZZA_MENU: PizzaItem[] = [
   {
@@ -6,7 +10,7 @@ export const PIZZA_MENU: PizzaItem[] = [
     name: 'Margherita',
     description: 'Tomate San Marzano, mozzarella di bufala, albahaca fresca y AOVE.',
     price: 12.50,
-    image: 'https://upload.wikimedia.org/wikipedia/commons/f/f4/Pizza.png',
+    image: margheritaImg,
     label: 'VEGGIE',
     defaultIngredients: ['Tomate San Marzano', 'Mozzarella di bufala', 'Albahaca fresca', 'AOVE']
   },
@@ -15,7 +19,7 @@ export const PIZZA_MENU: PizzaItem[] = [
     name: 'Pepperoni',
     description: 'Pepperoni artesanal ahumado, mozzarella, miel picante y orégano.',
     price: 14.00,
-    image: 'https://upload.wikimedia.org/wikipedia/commons/4/42/Pizza_Quattro_Stagioni_transparent.png',
+    image: pepperoniImg,
     bestSeller: true,
     defaultIngredients: ['Pepperoni artesanal ahumado', 'Mozzarella', 'Miel picante', 'Orégano']
   },
@@ -24,7 +28,7 @@ export const PIZZA_MENU: PizzaItem[] = [
     name: 'Carbonara',
     description: 'Base blanca de pecorino romano, guanciale crujiente y pimienta negra.',
     price: 15.50,
-    image: 'https://upload.wikimedia.org/wikipedia/commons/5/5f/Spinach_pizza.png',
+    image: carbonaraImg,
     defaultIngredients: ['Base de pecorino romano', 'Guanciale crujiente', 'Yema de huevo', 'Pimienta negra']
   },
   {
@@ -32,7 +36,7 @@ export const PIZZA_MENU: PizzaItem[] = [
     name: 'Vegana',
     description: 'Pimientos asados, cebolla caramelizada, champiñones y queso vegano.',
     price: 13.50,
-    image: 'https://upload.wikimedia.org/wikipedia/commons/f/f4/Pizza.png',
+    image: veganaImg,
     label: 'VEGAN',
     defaultIngredients: ['Pimientos asados', 'Cebolla caramelizada', 'Champiñones', 'Queso vegano artesanal']
   }
@@ -83,7 +87,7 @@ export const REVIEWS_DATA: ReviewItem[] = [
 ];
 
 export const IMAGES_RESOURCES = {
-  heroPizza: 'https://upload.wikimedia.org/wikipedia/commons/4/42/Pizza_Quattro_Stagioni_transparent.png',
-  storyPepperoni: 'https://upload.wikimedia.org/wikipedia/commons/4/42/Pizza_Quattro_Stagioni_transparent.png',
+  heroPizza: pepperoniImg,
+  storyPepperoni: pepperoniImg,
   chefCut: 'https://lh3.googleusercontent.com/aida-public/AB6AXuCM8KweoU8iyd8mZtFXCDalactmbY6iYeXN5jkF8UYMoY-a3pqXqStjhYSJwSR7BrOiPjcvQsZ_3GcUmJ1B9M_VjrfhQZ4ejzHFEy76WC2QM8HrA8Q8WmcaceGrnKX63OXIt_K5eawaaBIIYw9-R0SutKBN_NeUscWqovpnxsQVFpYy2AmyTOaFLdZVX9088XgOvqYayWZoQZ3-DKRwBFUk3KEe2bQ0H24-CXQTO1drkT61S0i_QPfI2i7lrPOgnyrSM7RKgSIVT0s'
 };
